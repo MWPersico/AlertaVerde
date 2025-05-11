6d4a1d465824e11e47d88bd9db52b16ae2a7335e
MWPersico <willian.marcos7@hotmail.com>
2025-05-11 20:02:46 -0300

    project backend created

---
+++ backend/src/main/java/br/senac/ead/alerta_verde/AlertaVerdeApplication.java
@@ -0,0 +1,13 @@
+package br.senac.ead.alerta_verde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertaVerdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertaVerdeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/senac/ead/alerta_verde/AlertaVerdeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senac.ead.alerta_verde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlertaVerdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
